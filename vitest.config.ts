3051659e8480a68a6159713c02aa071cae427883
venuyadavalli <venuyadavalli4@gmail.com>
2026-02-11 00:02:40 +0530

    changed config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: "/smile-weaver/", 
   plugins: [react()],
   test: {
     environment: "jsdom",
